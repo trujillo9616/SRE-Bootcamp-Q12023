451945940cc004646401ea98aab9948f3b3ba2d5
Adrian Trujillo <adrian.td96@gmail.com>
2023-02-02 00:53:23 -0600

    refactor: :recycle: refactor code with updated routes and middleware

--- auth_api/node/src/server.ts
+++ auth_api/node/src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request } from 'express';
 import morgan from 'morgan';
-import * as routes from './routes';
+import { loginRouter, protectedRouter, healthRouter } from './routes';
+import middleware from './utils/middleware';
 
 const app = express();
 
@@ -8,6 +9,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 morgan.token('body', (req: Request) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
-routes.init(app);
+
+// Routes
+app.use('/api/login', loginRouter);
+app.use('/api/protected', protectedRouter);
+app.use('/api/_health', healthRouter);
+
+// Middleware
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 export default app;

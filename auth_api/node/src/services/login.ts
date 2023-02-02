f6869e4d5c42d01dba92ed0feec5119e193f9910
Adrian Trujillo <adrian.td96@gmail.com>
2023-02-02 00:49:37 -0600

    feat: :sparkles: added the correct functionality for the loginService
    Implemented the login service function to correctly authenticate users.

--- auth_api/node/src/services/login.ts
+++ auth_api/node/src/services/login.ts
@@ -1,3 +1,39 @@
-export const loginFunction = (_username: string, _password: string) => {
-  return 'test';
+import { prisma } from '../utils/prisma';
+import crypto from 'crypto';
+import jwt from 'jsonwebtoken';
+import exclude from '../utils/helper';
+import config from '../config/config';
+
+export const loginFunction = async (username: string, password: string, expiration?: string) => {
+  expiration = expiration || '1h';
+  try {
+    const user = await prisma.users.findUnique({
+      where: {
+        username: username,
+      }
+    });
+
+    if (!user) {
+      const error = new Error('User not found, please check your username.');
+      error.name = 'UserNotFound';
+      throw error;
+    }
+
+    const hash = crypto.createHash('sha512')
+      .update(password + user.salt!)
+      .digest('hex');
+
+    if (hash !== user.password) {
+      const error = new Error('Invalid password, please check your password.');
+      error.name = 'InvalidPassword';
+      throw error;
+    }
+
+    const data = exclude(user, ['password', 'salt']);
+    const token = jwt.sign(data, config.jwt_secret, { expiresIn: expiration });
+    return token;
+
+  } catch (error) {
+    throw error;
+  }
 }

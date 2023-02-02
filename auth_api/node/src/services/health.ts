0f419ac87bf393a3a1408dfd0a1c0ac9f79b3a05
Adrian Trujillo <adrian.td96@gmail.com>
2023-02-02 00:38:28 -0600

    fix: :fire: removed unnecessary files
    Removed files that were no longer needed in the project.

--- auth_api/node/src/services/health.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-
-export const health = (_req: Request, res: Response, next: NextFunction) => {
-    res.send('OK');
-    next();
-}
--- auth_api/node/src/typings/express/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare namespace Express {
-  interface Request {
-    token?: string;
-    user?: string | object | null;
-  }
-}

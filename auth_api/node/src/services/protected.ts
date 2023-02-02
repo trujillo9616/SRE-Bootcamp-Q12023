9edfa4253e4a5b9382288f1376bdde180ec0a2f0
Adrian Trujillo <adrian.td96@gmail.com>
2023-02-02 00:51:22 -0600

    feat: :sparkles: implemented the functionality of the protect function.
    Implemented the protect service function for correctly authorizing tokens.

--- auth_api/node/src/services/protected.ts
+++ auth_api/node/src/services/protected.ts
@@ -1,3 +1,17 @@
-export const protectFunction = (_authorization: string) => {
-  return 'test';
+import jwt from 'jsonwebtoken';
+import config from '../config/config';
+
+export const protectFunction = (authorization: string) => {
+  try {
+    const token = authorization.split(' ')[1];
+    const decoded = jwt.verify(token, config.jwt_secret);
+    if (!decoded || typeof decoded === 'string') {
+      const error = new Error('Invalid token');
+      error.name = 'InvalidTokenError';
+      throw error;
+    }
+    return decoded;
+  } catch (error) {
+    throw error;
+  }
 }
